e06f036dc9e11d54c49add917179bb25cf842cd5
Lucas Pimentel <lucaspimentel@gmail.com>
2025-03-12 19:29:21 -0400

    initial commit

---
+++ Arguments.cs
@@ -0,0 +1,29 @@
+﻿namespace Preview.Image.Cli;
+
+public readonly struct Arguments(string? path, int? maxWidth)
+{
+    private const string MaxWidthArgName = "--max-width=";
+
+    public readonly string? Path = path;
+    public readonly int? MaxWidth = maxWidth;
+
+    public static Arguments Parse(string[] args)
+    {
+        string? path = null;
+        int? maxWidth = null;
+
+        foreach (var arg in args)
+        {
+            if (arg.StartsWith(MaxWidthArgName) && int.TryParse(arg.AsSpan(MaxWidthArgName.Length), out var width))
+            {
+                maxWidth = width;
+            }
+            else
+            {
+                path = arg;
+            }
+        }
+
+        return new Arguments(path, maxWidth);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Preview.Image.Cli;
+using Spectre.Console;
+
+var arguments = Arguments.Parse(args);
+
+if (arguments.Path is null)
+{
+    AnsiConsole.MarkupLine("[red]Please provide a path to an image file.[/]");
+    return;
+}
+
+if (!File.Exists(arguments.Path))
+{
+    AnsiConsole.MarkupLine("[red]The specified file does not exist.[/]");
+    return;
+}
+
+var image = new CanvasImage(arguments.Path)
+{
+    MaxWidth = arguments.MaxWidth,
+};
+
+AnsiConsole.Write(image);
